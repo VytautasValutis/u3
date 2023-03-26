52c045b05492f8cbe50a14289825eae979b3fcb0
Vytautas Valutis <dnc908@gmail.com>
2023-03-26 16:54:08 +0300

    INIT sctrusture w bootstrap

---
+++ views/top.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="http://u3.lt/app.js"></script>
+    <link rel="stylesheet" href="http://u3.lt/app.css">
+    <title>Document</title>
+</head>
+<body>
