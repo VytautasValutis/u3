3c9d479944274399192808891d6689873836571b
Vytautas Valutis <dnc908@gmail.com>
2023-04-01 15:15:04 +0300

    Fin Stage1

--- App/App.php
+++ App/App.php
@@ -24,6 +24,10 @@ class App {
             return (new HomeController)->home();
         } 
 
+        if($method == 'POST' && count($url) == 1 && $url[0] === 'logout') {
+            return (new LoginController)->logout();
+        } 
+
         if($method == 'GET' && count($url) == 1 && $url[0] === 'login') {
             return (new LoginController)->show();
         } 
--- views/clients/remval.php
+++ views/clients/remval.php
@@ -12,34 +12,34 @@ use App\Services\PersCode;
   <div class="row justify-content-center">
     <div class="col-8 ">
     <div class="card mt-5">
-        <div class="card-header">
+        <div class="card-header lentele-bg">
             <h1>Nurašyti lėšas</h1>
         </div>
             <div class="card-body">
                 <form action="<?= URL ?>list/remVal/<?= $client['id'] ?>" method="post">
                     <div class="mb-3">
-                        <label class="form-label fs-4">Kliento vardas</label>
-                        <input readonly type="text" class="form-control" name="name" value="<?= $client['name'] ?? ''?>">
+                        <label class="form-label fs-4 w-auto">Kliento vardas</label>
+                        <input readonly type="text" class="form-control w-50 d-inline-block float-end" name="name" value="<?= $client['name'] ?? ''?>">
                     </div>
                     <div class="mb-3">
-                        <label class="form-label fs-4">Kliento pavardė</label>
-                        <input readonly type="text" class="form-control" name="surname" value="<?= $client['surname'] ?? '' ?>">
+                        <label class="form-label fs-4 w-auto">Kliento pavardė</label>
+                        <input readonly type="text" class="form-control w-50 d-inline-block float-end" name="surname" value="<?= $client['surname'] ?? '' ?>">
                     </div>
                     <div class="mb-3">
-                        <label class="form-label fs-4">Kliento asmens kodas</label>
-                        <input readonly type="text" class="form-control" name="persCode" value="<?= $client['persCode'] ?? ""?>">
+                        <label class="form-label fs-4 w-auto">Kliento asmens kodas</label>
+                        <input readonly type="text" class="form-control w-50 d-inline-block float-end" name="persCode" value="<?= $client['persCode'] ?? ""?>">
                     </div>
                     <div class="mb-3">
-                        <label class="form-label fs-4">Kliento sąskaita</label>
-                        <input readonly type="text" class="form-control" name="accNum" value="<?= $client['accNum'] ?>">
+                        <label class="form-label fs-4 w-auto">Kliento sąskaita</label>
+                        <input readonly type="text" class="form-control w-50 d-inline-block float-end" name="accNum" value="<?= $client['accNum'] ?>">
                     </div>
                     <div class="mb-3">
-                        <label class="form-label fs-4">Kliento sąskaitos likutis</label>
-                        <input readonly type="text" class="form-control" name="value" value="<?= $client['value'] ?>">
+                        <label class="form-label fs-4 w-auto">Kliento sąskaitos likutis</label>
+                        <input readonly type="text" class="form-control w-50 d-inline-block float-end" name="value" value="<?= $client['value'] ?>">
                     </div>
                     <div class="mb-3">
-                        <label class="form-label fs-4">Nurodykite nurašomų lėšų kiekį</label>
-                        <input type="text" class="form-control" name="remValue" value="0">
+                        <label class="form-label fs-4 w-auto">Nurodykite nurašomų lėšų kiekį</label>
+                        <input type="text" class="form-control w-50 d-inline-block float-end" name="remValue" value="0">
                     </div>
                     <button type="submit" class="btn btn-primary">Patvirtinti</button>
                 </form>
